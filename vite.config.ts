3d4fbf6669f830e877b651359ebf880f226280a4
허용석 <johnsonheo96@gmail.com>
2025-04-09 16:29:23 +0900

    [#97] FEAT: 매물 페이지 상세 정보 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  define: {
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL)
+  }
 })
